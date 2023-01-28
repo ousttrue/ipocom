696efb3d941a526fb4584b4036768db20bc549a4
ousttrue <ousttrue@gmail.com>
2023-01-28 17:43:07 +0900

    fix time

--- Assets/Ipocom/Runtime/IpocomReceiver.cs
+++ Assets/Ipocom/Runtime/IpocomReceiver.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Ipocom
@@ -15,6 +17,16 @@ namespace Ipocom
         /// UDP 受信スレッドからメインスレッドデータを渡すのに使う
         /// </summary>
         ConcurrentQueue<object> m_queue = new ConcurrentQueue<object>();
+        int m_skeletonCount = 0;
+        int m_frameCount = 0;
+        uint m_frameNumber = 0;
+        uint m_maxFrame = 0;
+        List<float> m_receiveTimes = new List<float>();
+        float m_frameTime = 0;
+        const int MAX_TIMES = 60;
+        float m_frameRate = 0;
+        int m_lastTime = 0;
+        float m_maxTime = 0;
 
         void OnEnable()
         {
@@ -52,17 +64,75 @@ namespace Ipocom
                 switch (data)
                 {
                     case SonyMotionFormat.SkeletonMessage skeleton:
+                        ++m_skeletonCount;
                         Debug.Log($"{skeleton}");
                         m_onSkeleton.Invoke(skeleton);
                         break;
 
                     case SonyMotionFormat.FrameMessage frame:
+                        ++m_frameCount;
+                        if (m_receiveTimes.Count > 0 && m_frameTime > frame.fram.Time)
+                        {
+                            // Rewind
+                            Debug.Log("rewind");
+                            m_maxTime = Mathf.Max(m_maxTime, m_frameTime);
+                            m_maxFrame = Math.Max(m_maxFrame, m_frameNumber);
+                            m_receiveTimes.Clear();
+                        }
+                        m_frameNumber = frame.fram.FrameNumber;
+                        m_frameTime = frame.fram.Time;
+                        var time = UnityEngine.Time.time;
+                        m_receiveTimes.Add(time);
+                        if (m_receiveTimes.Count > MAX_TIMES)
+                        {
+                            m_receiveTimes.RemoveRange(0, m_receiveTimes.Count - MAX_TIMES);
+                        }
                         m_onFrame.Invoke(frame);
                         break;
 
                     default:
                         throw new ArgumentException("unknown");
                 }
+            }
+        }
+
+        void OnGUI()
+        {
+            // skeleton count
+            GUILayout.Label($"received skeleton: {m_skeletonCount}");
+            GUILayout.Label($"received frame: {m_frameCount}");
+            if (m_frameTime < m_maxTime)
+            {
+                GUILayout.Label($"frame time: {m_frameTime:.00}/{m_maxTime:.00}");
+            }
+            else
+            {
+                GUILayout.Label($"frame time: {m_frameTime:.00}");
+            }
+            if (m_frameNumber < m_maxFrame)
+            {
+                GUILayout.Label($"frame no: {m_frameNumber}/{m_maxFrame}");
+            }
+            else
+            {
+                GUILayout.Label($"frame no: {m_frameNumber}");
+            }
+            // frame rate
+            if (m_receiveTimes.Count == MAX_TIMES)
+            {
+                var current = (int)Time.time;
+                if (current != m_lastTime)
+                {
+                    m_lastTime = current;
+                    float delta = m_receiveTimes.Last() - m_receiveTimes.First();
+                    // for (int i = 1; i < m_frameTimes.Count; ++i)
+                    // {
+                    //     delta += m_frameTimes[i] - m_frameTimes[i - 1];
+                    // }
+                    delta /= (m_receiveTimes.Count - 1);
+                    m_frameRate = 1 / delta;
+                }
+                GUILayout.Label($"fps: {m_frameRate}");
             }
         }
     }
--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Yet another receiver
 ```
     "jp.ousttrue.ipocom": "https://github.com/ousttrue/ipocom.git?path=/Assets/Ipocom#v2.1.0"
 ```
+
+## protocol
+
+https://github.com/seagetch/mcp-receiver/blob/main/doc/Protocol.md
